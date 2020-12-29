d1de2fc53c4c465467de337c569157bffb9d4b0f
Sam Cutler <sam.cutler@gmail.com>
2020-12-29 15:21:12 -0800

    Beginning work on the Blogful project

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,28 @@
+BEGIN;
+
+INSERT INTO blogful_articles 
+    (title, date_published, content)
+VALUES
+    ('Article 1', '2016-01-01', 'Blah blah blah blah.'),
+    ('Second article', '2016-01-10', 'Blahdee blahdee blahdee blahrg.'),
+    ('Fourth', '2016-03-11', 'ASLKFJ j daslfkjiof lkmaiefjklmaf aioef jlkfjaif  ofejf ljfkj aeof'),
+    ('5', '2016-05-19', 'lkjs sflkjalskfjslka fl ksfjlasfjls fjalkfjalksfjsadlkfja'),
+    ('Seis', '2013-02-12', 'lkjsaf lkajflkj faljfiowflkaf akfjeqkmapacnm afsd'),
+    ('America', '2018-04-12', 'A lkjsfl eiwji foifwjf wq jfoi joiwq jfoj'),
+    ('Canada', '2013-02-02', 'Plkaj  alksfjlkafj oifjiwoejf lkafjaifjwoijf '),
+    ('Senate foreign relations', '2016-03-11', 'lkjwf jifj iaofjkla fjaklsfmlkejfoeiqwlkamckcma'),
+    ('Russia', '2019-10-08', 'lkjd lkfowiqf klfjlkafjoiqwjf oimflkamfklmdqoifqm  asdf'),
+    ('Housing', '2019-11-11', 'A asfkljaslfk jafueqwfoiewnf sa f'),
+    ('Finance', '2016-06-16', 'NCiojlj dqijfioweqmnicoam iowefwmnf ljfalk jlsdfa'),
+    ('Master of the Senate', '2016-03-11', 'SLfjqkwelefj oijfklasjf klasjfklawf'),
+    ('Home ownership', '2016-03-11', 'SDKLjlkwd jflkjaflkjqw oi;efjasklfj alfkj'),
+    ('Skiing and snowboarding', '2016-03-11', 'asfkj ewlkqfjioqwflsakdmfklas dfmklaewf'),
+    ('Backpacking', '2020-06-01', 'alskfdm qewjiomadfkl mqofiemqklsadfm'),
+    ('Rock Climbing', '2016-12-12', 'qoiejklsmfkls jfoieqmwklasdmf'),
+    ('Rock Climbing Washington', '2019-02-01', 'lkmeqw klfmoifjeqlkmasd klfjaw;oifj'),
+    ('Hikes of the Pacific Northwest', '2020-01-02', 'm eqkonfkdasfn ioeqwjflkafnmklas fmlksaf'),
+    ('Recipe ideas', '2017-10-11', 'Aaslkdfjlksafj klwfjkladjfioweqjlksadfjklsa jflksaff'),
+    ('Continuing education', '2015-02-22', 'Aasflkj alskfkl;askf ;lakfl;adkf l;asdkf;lasffasf'),
+    ('New Year''s Resolutions', '2016-09-09', 'Xkjlkwdjf isjfklasjflkas jflkjfskl');
+
+COMMIT;
